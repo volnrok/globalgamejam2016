5461346751e64a6f820ca96aa830fe537580679f
Alchemist <gilhulysean@gmail.com>
2016-01-30 08:31:48 -0500

    Created cursor which moves on input

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorController : MonoBehaviour {
+
+	public string playerid;
+	public float moveSpeed;
+
+	void Update () {
+		float horiz = Input.GetAxis ("Horizontal" + playerid);
+		float vert = Input.GetAxis ("Vertical" + playerid);
+
+		transform.Translate (new Vector3 (horiz * moveSpeed, vert * moveSpeed, 0));
+	}
+}
